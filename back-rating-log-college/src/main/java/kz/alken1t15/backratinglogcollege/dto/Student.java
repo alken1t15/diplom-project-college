cbb68263f5948aad3e1bb58fdb16087b034d9082
Alexey <alken1t15@gmail.com>
2023-09-08 21:17:08 +0600

    one dto student

---
+++ back-rating-log-college/src/main/java/kz/alken1t15/backratinglogcollege/dto/Student.java
@@ -0,0 +1,27 @@
+package kz.alken1t15.backratinglogcollege.dto;
+
+import jakarta.persistence.Column;
+import kz.alken1t15.backratinglogcollege.entity.Groups;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Student {
+
+    private Groups group;
+
+    private String firstName;
+
+    private String secondName;
+
+    private String middleName;
+
+    private LocalDate bornDate;
+}
