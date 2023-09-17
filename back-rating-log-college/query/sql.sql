1b82aaf0c7ae4352130d61e8dacdc503cc31fb7a
Alexey <alken1t15@gmail.com>
2023-09-17 23:52:06 +0600

    add query

--- back-rating-log-college/query/sql.sql
+++ back-rating-log-college/query/sql.sql
@@ -3,15 +3,22 @@ create table objects (
   name varchar(255) not null
 );
 
+insert into objects (name) value ('АиП'),('ООП'),('Веб программирование'),('Стандартизации'),('Основы философии'),('Методы защиты информации'),('Физкультура')
+,('Моделирование');
+
 create table week(
     id serial primary key ,
     name varchar(255) not null
 );
 
+insert into week(name)value ('Понедельник'),('Вторник'),('Среда'),('Четверг'),('Пятница'),('Суббота');
+
 create table groups(
   id serial primary key,
   name varchar(255) not null
 );
+
+insert into groups (name) value ('П-20-51б');
 
 create table group_chart(
   id serial primary key,
@@ -21,6 +28,7 @@ create table group_chart(
   date_start date not null,
   date_end date not null
 );
+insert into group_chart (id_group, curse, name, date_start, date_end) values (1,3,'Учеба','1.09.2023','25.10.2023');
 
 
 create table schedule(
@@ -30,6 +38,8 @@ create table schedule(
   id_week int references  week(id),
   couple int not null
 );
+
+insert into schedule (id_object, id_group_chart, id_week, couple) values (1,1,1,2),(2,1,1,3),(1,1,1,3);
 
 create table students(
   id serial primary key,
@@ -42,6 +52,8 @@ create table students(
   born_date date not null
 );
 
+insert into students (id_group, first_name, second_name, middle_name, login, password, born_date) values (1,'Максим','Кораблев','Игорович',
+                                                                                                          'kramzos222','maxim22','2.04.2004')
 create table evaluations(
   id serial primary key,
   id_student int references students(id),
