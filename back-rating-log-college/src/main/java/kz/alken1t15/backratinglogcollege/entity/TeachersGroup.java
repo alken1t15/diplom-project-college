d1864d455087b2505fb4a62205b228bf06477aeb
Alexey <alken1t15@gmail.com>
2023-10-24 13:28:40 -0600

    fix 2.0

--- back-rating-log-college/src/main/java/kz/alken1t15/backratinglogcollege/entity/TeachersGroup.java
+++ back-rating-log-college/src/main/java/kz/alken1t15/backratinglogcollege/entity/TeachersGroup.java
@@ -2,6 +2,7 @@ package kz.alken1t15.backratinglogcollege.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
@@ -10,6 +11,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class TeachersGroup {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
