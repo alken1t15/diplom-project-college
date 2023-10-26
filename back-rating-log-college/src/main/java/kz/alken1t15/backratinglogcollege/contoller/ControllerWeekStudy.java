d3fcb09226a6fc256c1572311b09366d8be2f6db
Alexey <alken1t15@gmail.com>
2023-10-26 23:41:42 +0600

    add new controller

---
+++ back-rating-log-college/src/main/java/kz/alken1t15/backratinglogcollege/contoller/ControllerWeekStudy.java
@@ -0,0 +1,11 @@
+package kz.alken1t15.backratinglogcollege.contoller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/week/study")
+public class ControllerWeekStudy {
+}
