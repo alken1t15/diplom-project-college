a4921276a19ad0e40a7ac64acd761f8f32dbe689
alken1t15 <alken1t15@gmail.com>
2024-04-07 23:57:35 +0500

    fix

--- back-rating-log-college/src/main/java/kz/alken1t15/backratinglogcollege/security/JWTFilter.java
+++ back-rating-log-college/src/main/java/kz/alken1t15/backratinglogcollege/security/JWTFilter.java
@@ -84,16 +84,16 @@ public class JWTFilter extends OncePerRequestFilter {
                 }
             }
         } else {
-            if (request.getRequestURL().equals("http://localhost:8080/login/jwt")) {
+//            if (request.getRequestURL().equals("http://localhost:8080/login/jwt")) {
                 filterChain.doFilter(request, response);
                 return;
-            }else {
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                response.getWriter().write("Вы не ввели JWT токен");
-                response.setCharacterEncoding("UTF-8");
-                response.getWriter().flush();
-                response.getWriter().close();
-            }
+//            }else {
+//                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+//                response.getWriter().write("Вы не ввели JWT токен");
+//                response.setCharacterEncoding("UTF-8");
+//                response.getWriter().flush();
+//                response.getWriter().close();
+//            }
         }
     }
 }
