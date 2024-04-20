693649558cabd75a1f8cbaa7e44f66fb4e5d7783
alken1t15 <alken1t15@gmail.com>
2024-04-20 21:44:10 +0500

    update save file

--- back-rating-log-college/src/main/java/kz/alken1t15/backratinglogcollege/contoller/TestCont.java
+++ back-rating-log-college/src/main/java/kz/alken1t15/backratinglogcollege/contoller/TestCont.java
@@ -4,6 +4,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 @RestController
 @RequestMapping("/file")
 public class TestCont {
@@ -14,5 +19,15 @@ public class TestCont {
         System.out.println(file.getName());
         System.out.println(file.getSize());
         System.out.println(file.getContentType());
+        if (!file.isEmpty()){
+            try {
+                byte[] bytes = file.getBytes();
+                System.out.println(file.getOriginalFilename());
+                File saveFile = new File(file.getOriginalFilename());
+                file.transferTo(saveFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
